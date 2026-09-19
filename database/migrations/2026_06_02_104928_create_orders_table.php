98bc4a2b59c2dce5929d1b4a19fd5881bf5d2674
Webcare-it <dev.minhajul@gmail.com>
2026-09-19 14:11:38 +0600

    [FIX] fraud checker

--- database/migrations/2026_06_02_104928_create_orders_table.php
+++ database/migrations/2026_06_02_104928_create_orders_table.php
@@ -24,6 +24,7 @@ class CreateOrdersTable extends Migration
             $table->string('delivery_status')->default('pending');
 
             $table->string('is_otp_verified')->nullable();
+            $table->string('is_verified_fraud')->nullable(); // fraud, not_verified, verified
             $table->string('payment_type')->nullable();
             $table->boolean('manual_payment')->default(false);
             $table->longText('manual_payment_data')->nullable();
